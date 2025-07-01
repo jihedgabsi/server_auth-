de3882becac9ee7425f943f9aa39b950113b6cc2
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-07-01 16:19:09 +0100

    Update Traject.js

--- models/Traject.js
+++ models/Traject.js
@@ -2,6 +2,10 @@
 const mongoose = require('mongoose');
 
 const trajectSchema = new mongoose.Schema({
+   id: {
+    type: String,
+    unique: true
+  },
    idChauffeur: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Driver'
