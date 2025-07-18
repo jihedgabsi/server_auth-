3976dff556cfe30b21714b3619873f179012885c
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-07-18 10:40:31 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    url: process.env.MONGODB_URI '
+    url: process.env.MONGODB_URI || '',
   
   };
   
   // ===== config/config.js =====
   module.exports = {
-  IMAGE_SERVER_URL: process.env.IMAGE_SERVER_URL || 'http://m4ckwwswggwo8c8g08gwsscc.82.112.242.233.sslip.io/uploads/upload',
+  IMAGE_SERVER_URL: process.env.IMAGE_SERVER_URL || '',
     jwtSecret: process.env.JWT_SECRET || 'your-secret-key',
     jwtExpiration: 86400, // 24 hours (in seconds)
     email: {
@@ -18,6 +18,10 @@ module.exports = {
       },
       from: process.env.EMAIL_FROM || 'noreply@example.com'
     },
+
+    whatsappApi: {
+    baseUrl: process.env.WHATSAPP_API_BASE_URL || 'http://localhost:3001'
+  },
     resetPasswordExpiry: 3600000, // 1 hour (in milliseconds)
     verificationCodeExpiry: 3600000, // 1 hour (in milliseconds)
     verificationCodeLength: 6 // Length of verification code
