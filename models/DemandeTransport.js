2a5d1164acf47ef621b54e4b3e6e6f0d616e6bb2
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-08-15 09:52:39 +0100

    Update DemandeTransport.js

--- models/DemandeTransport.js
+++ models/DemandeTransport.js
@@ -61,9 +61,6 @@ const demandeTransportSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-    Percentagecommisionactuel: {
-    type: Number,
-  },
   id_bagages: [{ // Corrected from id_bagagdes to id_bagages for consistency
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Baggage', // Referencing the existing Baggage model
